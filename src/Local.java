a99ec729e773bd0b9d0f1e6e0349d2ecafc936ea
subha-code <64766123+subha-code@users.noreply.github.com>
2024-03-04 21:37:31 +0530

    Add files via upload

---
+++ src/Local.java
@@ -0,0 +1,13 @@
+public class Local {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+       /* A a1 = new A();
+        a1.test();
+    }*/
+  /*  public void test(){
+
+        System.out.println(x);
+    }*/
+    }
+}
