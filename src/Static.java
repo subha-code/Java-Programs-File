830f615181408713a344b1cd6b0b4a6e8d16df96
subha-code <64766123+subha-code@users.noreply.github.com>
2024-03-04 21:38:07 +0530

    Add files via upload

---
+++ src/Static.java
@@ -0,0 +1,12 @@
+//Static variable can access anywhere
+public class Static{
+    static int x = 10;
+    public static void main(String[] args){
+        System.out.println(A.x);
+        A a1=new A();
+        a1.test();
+    }
+    public void test(
+            System.out.println(A.x);
+    )
+}
