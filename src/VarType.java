5240b5fc5c1634b7eaf5c95d87f64fafa6f5a963
subha-code <64766123+subha-code@users.noreply.github.com>
2024-03-06 22:51:05 +0530

    Add files via upload

---
+++ src/VarType.java
@@ -0,0 +1,31 @@
+public class VarType{
+    public void value() {
+
+        var variable1 = 10;
+
+        var variable2 = "Java";
+
+        var variable3 = "7.5f";
+
+        var variable4 = true;
+
+        System.out.println(variable1); // print 10
+
+        System.out.println(variable2); // print Java
+
+        System.out.println(variable3); // print 7.5
+
+        System.out.println(variable4); // print true
+    }
+
+    public static void main(String[ ] args){
+
+
+
+        VarType obj = new VarType();
+
+        obj.value();
+
+    }
+
+}
